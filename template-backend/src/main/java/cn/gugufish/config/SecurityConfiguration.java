88aa4c73ff93ab629bc531fcef0af1ee7a01d32a
一只咕咕鱼 <1312255201@qq.com>
2025-03-12 01:18:32 +0800

    完成JWT
    完成JWT验证
    完成Redis JWT拉黑操作

--- template-backend/src/main/java/cn/gugufish/config/SecurityConfiguration.java
+++ template-backend/src/main/java/cn/gugufish/config/SecurityConfiguration.java
@@ -1,22 +1,34 @@
 package cn.gugufish.config;
 
 import cn.gugufish.entity.RestBean;
+import cn.gugufish.entity.vo.response.AuthorizeVO;
+import cn.gugufish.filter.JwtAuthorizeFilter;
+import cn.gugufish.utils.JwtUtils;
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Configuration
 public class SecurityConfiguration {
+    @Resource
+    JwtUtils jwtUtils;
+    @Resource
+    JwtAuthorizeFilter  jwtAuthorizeFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -33,28 +45,61 @@ public class SecurityConfiguration {
                         .logoutUrl("/api/auth/logout")
                         .logoutSuccessHandler(this::OnLogoutSuccess)
                 )
+                .exceptionHandling(conf -> conf
+                        .authenticationEntryPoint(this::onUnauthorized)
+                        .accessDeniedHandler(this::onAccessDenied)
+                )
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(conf -> conf
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .addFilterBefore(jwtAuthorizeFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+
     public void OnAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException {
         response.setContentType("application/json;charset=utf-8");
-        response.getWriter().write(RestBean.success().asJsonString());
-    }
-    public void OnAuthenticationFailure(HttpServletRequest request,
-                                        HttpServletResponse response,
-                                        AuthenticationException exception) throws IOException, ServletException {
-        response.setContentType("application/json;charset=utf-8");
-        response.getWriter().write(RestBean.failure(401,exception.getMessage()).asJsonString());
+        User user = (User)authentication.getPrincipal();
+        String token = jwtUtils.createJwt(user,1,"小明");
+        AuthorizeVO vo = new AuthorizeVO();
+        vo.setExpire(jwtUtils.expireTime());
+        vo.setRole("");
+        vo.setToken(token);
+        vo.setUsername("小明");
+        response.getWriter().write(RestBean.success(vo).asJsonString());
     }
     public void OnLogoutSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
-        response.getWriter().write("Success");
+                                        Authentication authentication) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        String authorization = request.getHeader("Authorization");
+        if(jwtUtils.invalidateJwt(authorization)){
+            writer.write(RestBean.success().asJsonString());
+        }
+        else{
+            writer.write(RestBean.failure(400,"退出登录失败").asJsonString());
+        }
+    }
+    public void OnAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(RestBean.unauthorized(exception.getMessage()).asJsonString());
+    }
+    public void onAccessDenied(HttpServletRequest request,
+                               HttpServletResponse response,
+                               AccessDeniedException exception) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(RestBean.forbidden(exception.getMessage()).asJsonString());
+    }
+    public void onUnauthorized(HttpServletRequest request,
+                               HttpServletResponse response,
+                               AuthenticationException exception) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(RestBean.unauthorized(exception.getMessage()).asJsonString());
     }
 }
---
+++ template-backend/src/main/java/cn/gugufish/utils/Const.java
@@ -0,0 +1,5 @@
+package cn.gugufish.utils;
+
+public class Const {
+    public static final String JWT_BLACK_LIST ="jwt:blacklist";
+}
