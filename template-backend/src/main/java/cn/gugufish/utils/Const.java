50f4f342cbf5891281d4ddd1588c5d8cb3b8f70d
一只咕咕鱼 <1312255201@qq.com>
2025-03-12 21:44:04 +0800

    跨域配置
    跨域配置

--- template-backend/src/main/java/cn/gugufish/utils/Const.java
+++ template-backend/src/main/java/cn/gugufish/utils/Const.java
@@ -2,4 +2,8 @@ package cn.gugufish.utils;
 
 public class Const {
     public static final String JWT_BLACK_LIST ="jwt:blacklist";
+    public final static int ORDER_FLOW_LIMIT = -101;
+    public static final int ORDER_CORS = -102;
+    public final static String FLOW_LIMIT_COUNTER = "flow:counter:";
+    public final static String FLOW_LIMIT_BLOCK = "flow:block:";
 }
